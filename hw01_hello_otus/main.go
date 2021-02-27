751bcce344447a8a37e3f63ff78ceacebdd7911b
Владимир Астраханцев <vladimirastrakhantsev@MacBook-Pro.local>
2021-02-27 23:51:52 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"golang.org/x/example/stringutil"
 )
 
 func main() {
 	fmt.Println(stringutil.Reverse("Hello, OTUS!"))
+	gg := os.Stdout
+	fmt.Println(gg)
+
 }
