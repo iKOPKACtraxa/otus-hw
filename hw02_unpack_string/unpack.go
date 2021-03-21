f908fbc6ecd34083cd3d8c2eadfdc4973ea3d2f0
iKOPKACtraxa <vvastrakhantsev@gmail.com>
2021-03-21 22:49:00 +0400

    HW1 is completed (part 2)

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,43 +9,37 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(inStr string) (string, error) {
-	var symbolToWrite string          // это символ, который нужно вывести
-	var stringToBuild strings.Builder // это строка к выводу
-	var flag bool                     // флаг экранирования
+	var symbolToWrite string
+	var stringToBuild strings.Builder
+	var flagOfEscapeExist bool
+	const escapeCharacter string = "\\"
 	for i, runeSymbol := range inStr {
-		symbol := string(runeSymbol)           // это символ, но пока не понятно, число это или буква
-		intSymbol, err := strconv.Atoi(symbol) // пытаемся получить из символа число
-		gotNumber := err == nil                // обозначает что удалось достать число
-		if gotNumber && symbolToWrite != "" {  // если получилось число и из предыдущей итерации есть символ для вывода...
-			if intSymbol > 0 { // ...и если нужно вывести >0 раз, то выводим intSymbol раз, иначе нет вывода (например для a0)
+		symbol := string(runeSymbol)
+		intSymbol, err := strconv.Atoi(symbol)
+		gotNumber := err == nil
+		if gotNumber && symbolToWrite != "" { // обычный случай, для вывода, например, a4 -> aaaa
+			if intSymbol > 0 { // условие нужно для того, чтобы не было вывода при 0, например, a0 -> ""
 				stringToBuild.WriteString(strings.Repeat(symbolToWrite, intSymbol))
 			}
-			// так как вывели символы "через распаковку", то переменную нужно очистить
-			// иначе она выведется на следующей итерации еще раз
 			symbolToWrite = ""
-		} else if gotNumber && symbolToWrite == "" { // если получилось число и если нет символа для вывода,
-			// сначала проверим был ли флаг, если был,
-			// значит это заэкранированное число и нужно передать symbol в symbolToWrite, и снять флаг
-			if flag {
+		} else if gotNumber && symbolToWrite == "" { // случаи "некорректных строк" или экранированных символов
+			if flagOfEscapeExist { // случай экранированных символов
 				symbolToWrite = symbol
-				flag = false
-			} else { // а если и флага не было, то это "некорректная строка"
+				flagOfEscapeExist = false
+			} else { // случай "некорректных строк"
 				return "", ErrInvalidString
 			}
 		}
-		if !gotNumber { // если текущий символ не число,
-			// значит из предыдущей итерации нужно вывести символ к выводу
-			stringToBuild.WriteString(symbolToWrite)
-			// и так как текущий символ не число,
-			// то мы сохраняем его к выводу на следующей итерации
-			symbolToWrite = symbol
-			// логика экранирования:
-			if symbol == "\\" { // если текущий символ "\"...
-				if !flag { // ...и флага не было, то поднимаем флаг и symbolToWrite=""
-					flag = true
+		if !gotNumber { // обычный случай, когда текущий символ не число, например ab -> ab
+			stringToBuild.WriteString(symbolToWrite) // нужно вывести сохраненный в прошлой итерации символ (a)
+			symbolToWrite = symbol                   // а текущий символ сохранить для следующей итерации (b)
+			// логика экранирования "\"
+			if symbol == escapeCharacter {
+				if !flagOfEscapeExist {
+					flagOfEscapeExist = true
 					symbolToWrite = ""
-				} else { // ...и флаг был то снимаем флаг
-					flag = false
+				} else {
+					flagOfEscapeExist = false
 				}
 			}
 		}
