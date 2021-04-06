6160e1e2a5c587378f8c171f5b8c4517a7bf3702
iKOPKACtraxa <vvastrakhantsev@gmail.com>
2021-04-06 22:37:09 +0300

    HW5 is NOT completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,8 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"fmt"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +12,22 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	var wg sync.WaitGroup
+
+	for _, v := range tasks {
+		wg.Add(1)
+		// time.Sleep(1 * time.Millisecond)
+		go func() {
+			defer wg.Done()
+			err := v()
+			if err != nil {
+				fmt.Println(err)
+			}
+		}()
+	}
+	// go func() {
+	wg.Wait()
+	fmt.Println("this is the end")
+	// }()
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -16,7 +16,7 @@ func TestRun(t *testing.T) {
 	defer goleak.VerifyNone(t)
 
 	t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
-		tasksCount := 50
+		tasksCount := 100
 		tasks := make([]Task, 0, tasksCount)
 
 		var runTasksCount int32
@@ -38,33 +38,33 @@ func TestRun(t *testing.T) {
 		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
 	})
 
-	t.Run("tasks without errors", func(t *testing.T) {
-		tasksCount := 50
-		tasks := make([]Task, 0, tasksCount)
+	// t.Run("tasks without errors", func(t *testing.T) {
+	// 	tasksCount := 50
+	// 	tasks := make([]Task, 0, tasksCount)
 
-		var runTasksCount int32
-		var sumTime time.Duration
+	// 	var runTasksCount int32
+	// 	var sumTime time.Duration
 
-		for i := 0; i < tasksCount; i++ {
-			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
-			sumTime += taskSleep
+	// 	for i := 0; i < tasksCount; i++ {
+	// 		taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+	// 		sumTime += taskSleep
 
-			tasks = append(tasks, func() error {
-				time.Sleep(taskSleep)
-				atomic.AddInt32(&runTasksCount, 1)
-				return nil
-			})
-		}
+	// 		tasks = append(tasks, func() error {
+	// 			time.Sleep(taskSleep)
+	// 			atomic.AddInt32(&runTasksCount, 1)
+	// 			return nil
+	// 		})
+	// 	}
 
-		workersCount := 5
-		maxErrorsCount := 1
+	// 	workersCount := 5
+	// 	maxErrorsCount := 1
 
-		start := time.Now()
-		err := Run(tasks, workersCount, maxErrorsCount)
-		elapsedTime := time.Since(start)
-		require.NoError(t, err)
+	// 	start := time.Now()
+	// 	err := Run(tasks, workersCount, maxErrorsCount)
+	// 	elapsedTime := time.Since(start)
+	// 	require.NoError(t, err)
 
-		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
-		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
-	})
+	// 	require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
+	// 	require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	// })
 }
