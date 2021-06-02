e1ef5096b1dfd7f4b4ae569bcb03d8c261cde6b1
iKOPKACtraxa <vvastrakhantsev@gmail.com>
2021-06-02 12:45:40 +0300

    HW8 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,31 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
-func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+// Also returns exit code of command and 1 if got a setenv problem.
+func RunCmd(cmd []string, env Environment) (returnCode int, err error) {
+	for k, v := range env {
+		if v.NeedRemove {
+			os.Unsetenv(k)
+			continue
+		}
+		err := os.Setenv(k, v.Value)
+		if err != nil {
+			return 1, fmt.Errorf("setenv at key:%v value:%v of environment from dir has got an error: %w", k, v.Value, err)
+		}
+	}
+	cmdToRun := exec.Command(cmd[2], cmd[3:]...) // #nosec G204
+	cmdToRun.Stdout = os.Stdout
+	err = cmdToRun.Run()
+	var p *exec.ExitError
+	if errors.As(err, &p) {
+		return err.(*exec.ExitError).ProcessState.ExitCode(), err // nolint:errorlint
+	}
+	return 0, nil
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,72 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	initEnv Environment = Environment{
+		"HELLO": EnvValue{Value: "SHOULD_REPLACE"},
+		"FOO":   EnvValue{Value: "SHOULD_REPLACE"},
+		"UNSET": EnvValue{Value: "SHOULD_REMOVE"},
+		"ADDED": EnvValue{Value: "from original env"},
+		"EMPTY": EnvValue{Value: "SHOULD_BE_EMPTY"},
+	}
+	osArgs            []string = []string{"go-envdir", "testdata/env", "bash", "testdata/echo.sh", "arg1=1", "arg2=2"}
+	osArgsForExitCode []string = []string{"go-envdir", "", "cd", "wrongPath"}
+	refString         string   = `HELLO is ("hello")
+BAR is (bar)
+FOO is (   foo
+with new line)
+UNSET is ()
+ADDED is (from original env)
+EMPTY is ()
+arguments are arg1=1 arg2=2
+`
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("Regular test", func(t *testing.T) {
+		for k, v := range initEnv {
+			err := os.Setenv(k, v.Value)
+			require.NoError(t, err, "setting of initEnv has got an error", err)
+		}
+
+		env, err := ReadDir(osArgs[1])
+		require.NoError(t, err, "reading of env files has got an error", err)
+
+		readerFromPipe, writerToPipe, err := os.Pipe()
+		require.NoError(t, err, "making of pipe has got an error", err)
+		realStdout := os.Stdout
+		os.Stdout = writerToPipe
+
+		returnCode, err := RunCmd(osArgs, env)
+		require.NoError(t, err, "RunCmd has got an error", err)
+		require.Truef(t, returnCode == 0, "return code is not a 0 it is: %v", returnCode)
+
+		outC := make(chan string)
+		go func() {
+			var buf bytes.Buffer
+			io.Copy(&buf, readerFromPipe)
+			readerFromPipe.Close()
+			outC <- buf.String()
+		}()
+		writerToPipe.Close()
+		os.Stdout = realStdout
+		outputOfRunCmd := <-outC
+		close(outC)
+		require.Truef(t, refString == outputOfRunCmd, "program output and reference string is not equal, needs:\n%v\ngot\n%v", refString, outputOfRunCmd)
+	})
+}
+
+func TestRunCmdForExitcodes(t *testing.T) {
+	t.Run("Test for exitcodes", func(t *testing.T) {
+		returnCode, _ := RunCmd(osArgsForExitCode, nil)
+		require.Truef(t, returnCode == 1, "return code is not 1")
+	})
 }
